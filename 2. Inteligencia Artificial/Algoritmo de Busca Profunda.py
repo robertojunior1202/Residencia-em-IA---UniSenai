1b4b7003b3210ed8b4dbf35dd47414685f8daace
Roberto Junior <83191322+robertojunior1202@users.noreply.github.com>
2025-03-03 22:02:33 -0300

    Update Algoritmo de Busca Profunda.py

--- 2. Inteligencia Artificial/Algoritmo de Busca Profunda.py
+++ 2. Inteligencia Artificial/Algoritmo de Busca Profunda.py
@@ -1,78 +1,43 @@
 import pandas as pd
-#from libs_roberto_junior import estrategiaAdjacencia
+from libs_roberto_junior import estrategiaAdjacencia
 #Importando Labirinto
 caminho = 'IA_2_labirinto256.txt'
 df = pd.read_csv(caminho, sep=" ", header=None)
 
-
-
-#implementar lista de posições visitadas
-#implementar pilha apenas casas que atendem a regra
-#deslocar para a melhor posição
-#marcar posição deslocada
-#captar dados das posições adjacentes
-
 #Parâmetros de Configuração
 posicao_atual = 0,0
-estrategia = "ea1"
-
-#Desmembrar depois
-def estrategiaAdjacencia(posicao_atual, estrategia):
-    #Regra de Posições
-    L = posicao_atual[0],posicao_atual[1]+1
-    SL = posicao_atual[0]+1,posicao_atual[1]+1
-    S = posicao_atual[0]+1,posicao_atual[1]
-    SO = posicao_atual[0]+1,posicao_atual[1]-1
-    O = posicao_atual[0],posicao_atual[1]-1
-    NO = posicao_atual[0]-1,posicao_atual[1]-1 
-    N = posicao_atual[0]-1,posicao_atual[1]
-    NL = posicao_atual[0]-1,posicao_atual[1]+1
-
-    #Regra de Estratégias
-    if estrategia == "ea1": #ea1 - SL-L-NL-N-NO-O-SO-S
-        regra = [SL,L,NL,N,NO,O,SO,S]
-        return regra
-    elif estrategia  == "ea2": #ea2 - S-SL-L-NL-N-NO-O-SO
-        regra = [S,SL,L,NL,N,NO,O,SO]
-        return regra
-    elif estrategia  == "ea3": #ea3 - L-SL-S-NL-N-SO-O-NO
-        regra = [L,SL,S,NL,N,SO,O,NO]
-        return regra
-    elif estrategia  == "ea4": #ea4 - SL-L-S-NL-N-SO-O-NO
-        regra = [SL,L,S,NL,N,SO,O,NO]
-        return regra
-    elif estrategia  == "ea5": #ea5 - SL-L-S-NL-N-SO-NO-O
-        regra = [SL,L,S,NL,N,SO,NO,O]
-        return regra
-    elif estrategia  == "ea6": #ea6 - SL-L-S-NL-N-O-NO-SO
-        regra = [SL,L,S,NL,N,O,NO,SO]
-        return regra
-    else:
-        return print(f'Estratégia {estrategia} é inválida. Escolha entre ea1 até ea6')
-
-regra = estrategiaAdjacencia(posicao_atual, estrategia)
-
+estrategia = "ea6"
 
 #Loop Infinito
 cont = 0
-pilha = []
+fila2 = []
 visitas = [[0,0]]
+encontrado = False
 while True:
+    fila = []
+    regra = estrategiaAdjacencia(posicao_atual, estrategia)  
     for posicao in regra:
         #Capta as informações adjacentes e armazena na pilha
         if 0<=posicao[0]<=255 and 0<=posicao[1]<=255:
             valor = df.iloc[posicao[0],posicao[1]]
-            if valor == 1:
-                pilha.append([posicao[0],posicao[1]])
-
+            if valor == -1:
+                print(f'Objetivo encontrado. O valor de -1 está nas posições {[posicao[0],posicao[1]]}')
+                encontrado = True
+            elif valor == 1:
+                fila.append([posicao[0],posicao[1]])
+    
+    if encontrado:
+        break
+    #Organizando os dados em pilha
+    for dado in reversed(fila):
+        if dado not in fila2:
+            fila2.append(dado)
+                
     #Organiza a movimentação
-    posicao_atual = pilha[0]  #capta próxima posição
-    pilha.remove(posicao_atual)
+    posicao_atual = fila2[-1]  #capta próxima posição
+    fila2.remove(posicao_atual)
     visitas.append(posicao_atual)
     df.iloc[posicao_atual[0],posicao_atual[1]]=255 #marca 255 na posição visitada  
-    regra = estrategiaAdjacencia(posicao_atual, estrategia)     
-     
-
-#implementar ajusta por pilha. Captar as posições e passar pra pilha     
-
-posicao = 0,0
+    cont+=1
+    
+print(f'O problema foi encontrado com {cont} iterações')
